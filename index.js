9dd539aa9f7b3ae33c0d10ad0f0bdb24d8500a1d
John-Anthony G Thevos <john.thevos@gmail.com>
2020-06-22 15:29:39 -0400

    moved the creation of osc client/server outside of the get

--- index.js
+++ index.js
@@ -24,38 +24,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
-	io.sockets.on('connection', function (socket) {
-		console.log('connection');
-
-		// when a connection is successful, create the OSC client and server
-		try {
-			socket.on("config", function (obj) {
-				isConnected = true;
-				console.dir(obj);
-		    	oscServer = new osc.Server(obj.server.port, obj.server.host);
-			    oscClient = new osc.Client(obj.client.host, obj.client.port);
-			    oscClient.send('/status', socket.sessionId + ' connected');
-				oscServer.on('message', function(msg, rinfo) {
-					socket.emit("message", msg);
-					console.dir(msg);
-				});
-				socket.emit("connected", 1);
-			});
-		 	socket.on("message", function (obj) {
-				oscClient.send.apply(oscClient, obj);
-		  	});
-			socket.on('disconnect', function(){
-				if (isConnected) {
-					oscServer.kill();
-					oscClient.kill();
-				}
-		  	});
-		// or catch the error
-		} catch (err) {
-			console.log(err.message);
-		}
-
-	});
-
 	res.status(200).send();
-})
+});
+
+io.sockets.on('connection', function (socket) {
+	console.log('connection');
+
+	// when a connection is successful, create the OSC client and server
+	try {
+		socket.on("config", function (obj) {
+			isConnected = true;
+			console.dir(obj);
+			oscServer = new osc.Server(obj.server.port, obj.server.host);
+			oscClient = new osc.Client(obj.client.host, obj.client.port);
+			oscClient.send('/status', socket.sessionId + ' connected');
+			oscServer.on('message', function(msg, rinfo) {
+				socket.emit("message", msg);
+				console.dir(msg);
+			});
+			socket.emit("connected", 1);
+		});
+		socket.on("message", function (obj) {
+			oscClient.send.apply(oscClient, obj);
+		});
+		socket.on('disconnect', function(){
+			if (isConnected) {
+				oscServer.kill();
+				oscClient.kill();
+			}
+		});
+	// or catch the error
+	} catch (err) {
+		console.log(err.message);
+	}
+
+});
