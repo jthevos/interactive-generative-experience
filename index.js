58c43525fe64b8d7161d606f4648ddc3e60b569e
John-Anthony G Thevos <john.thevos@gmail.com>
2020-06-22 12:38:36 -0400

    added path for get /

--- index.js
+++ index.js
@@ -23,33 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 // set the app to be served up at port 3000
 app.listen(3000);
 
-// when a connection is successful, create the OSC client and server
-io.sockets.on('connection', function (socket) {
-	console.log('connection');
-	try {
-		socket.on("config", function (obj) {
-			isConnected = true;
-			console.dir(obj);
-	    	oscServer = new osc.Server(obj.server.port, obj.server.host);
-		    oscClient = new osc.Client(obj.client.host, obj.client.port);
-		    oscClient.send('/status', socket.sessionId + ' connected');
-			oscServer.on('message', function(msg, rinfo) {
-				socket.emit("message", msg);
-				console.dir(msg);
+app.get('/', (req, res) => {
+	io.sockets.on('connection', function (socket) {
+		console.log('connection');
+		try {
+			socket.on("config", function (obj) {
+				isConnected = true;
+				console.dir(obj);
+		    	oscServer = new osc.Server(obj.server.port, obj.server.host);
+			    oscClient = new osc.Client(obj.client.host, obj.client.port);
+			    oscClient.send('/status', socket.sessionId + ' connected');
+				oscServer.on('message', function(msg, rinfo) {
+					socket.emit("message", msg);
+					console.dir(msg);
+				});
+				socket.emit("connected", 1);
 			});
-			socket.emit("connected", 1);
-		});
-	 	socket.on("message", function (obj) {
-			oscClient.send.apply(oscClient, obj);
-	  	});
-		socket.on('disconnect', function(){
-			if (isConnected) {
-				oscServer.kill();
-				oscClient.kill();
-			}
-	  	});
-	} catch (err) {
-		console.log(err.message);
-	}
+		 	socket.on("message", function (obj) {
+				oscClient.send.apply(oscClient, obj);
+		  	});
+			socket.on('disconnect', function(){
+				if (isConnected) {
+					oscServer.kill();
+					oscClient.kill();
+				}
+		  	});
+		} catch (err) {
+			console.log(err.message);
+		}
 
-});
+	});
+	
+	res.status(200).send();
+})
+
+// when a connection is successful, create the OSC client and server
